6a27f1942ccc870d0274744000f088b32a3819b9
paul9590 <paul9590@naver.com>
2023-05-27 18:10:26 +0900

    feature firebase init

--- android/app/src/main/java/com/mapletalk/MainApplication.java
+++ android/app/src/main/java/com/mapletalk/MainApplication.java
@@ -9,6 +9,7 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
 import java.util.List;
+import io.invertase.firebase.app.ReactNativeFirebaseAppPackage; // Firebase 패키지 추가
 
 public class MainApplication extends Application implements ReactApplication {
 
